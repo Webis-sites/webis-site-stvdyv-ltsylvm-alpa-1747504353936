f5c8a313882110fee265e8e200cf7d6b720f7bb5
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-05-17 21:21:52 +0300

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'סטודיו לצילום אלפא',
+  description: 'אנחנו סטודיו לצילום מוביל בתחום הצילום עם ניסיון של שנים רבות. אנחנו מתמחים במתן שירות מקצועי ואיכותי ללקוחותינו.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="he" dir="rtl">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#588C7E" />
+      </head>
+      <body className={inter.className}>
+        <div className="flex min-h-screen flex-col">
+          {/* Navbar will be inserted here */}
+
+          {children}
+
+
+          <footer className="py-6 bg-gray-100 dark:bg-gray-800 mt-10">
+            <div className="container mx-auto px-4 text-center text-gray-500 text-sm">
+              &copy; 2025 סטודיו לצילום אלפא. webis
+            </div>
+          </footer>
+        </div>
+      </body>
+    </html>
+  );
+}
